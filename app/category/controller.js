3b1bb41140706daa1399093666d56b5e35f9fad6
exca <excamuchlisandita@gmail.com>
2023-05-18 00:08:10 +0700

    (CRUD) Kategori - create

--- app/category/controller.js
+++ app/category/controller.js
@@ -1,3 +1,5 @@
+const Category = require('./model')
+
 module.exports = {
     index: async (req, res)=> {
         try {
@@ -5,5 +7,28 @@ module.exports = {
         } catch (err) {
             console.log(err)
         }
+    },
+
+    viewCreate : async (req, res)=> {
+        try {
+            res.render('admin/category/create')
+        } catch (err) {
+            console.log(err)
+        }
+    },
+
+    actionCreate : async (req, res)=> {
+        try {
+            const {name} = req.body
+
+            let category = await Category({name})
+            await category.save();
+
+            res.redirect('/category')
+        } catch (err) {
+            console.log(err)
+        }
     }
+
+
 }
