84cf0ec47672186d0e3c5ec3762f83f83c939cd0
exca <excamuchlisandita@gmail.com>
2023-05-21 00:31:33 +0700

    (CRUD) Voucher - Update Status Button

--- app/voucher/controller.js
+++ app/voucher/controller.js
@@ -195,7 +195,7 @@ module.exports = {
         try {
             const {id} = req.params;
             const voucher = await Voucher.findOne({_id: id})
-            
+
             await Voucher.findOneAndRemove({
                 _id: id
             });
@@ -209,6 +209,28 @@ module.exports = {
             req.flash('alertStatus', "success")
 
             res.redirect('/voucher');
+        } catch (err) {
+            req.flash('alertMessage', `${err.message}`)
+            req.flash('alertStatus', 'danger')
+            res.redirect('/voucher')
+        }
+    },
+
+    actionStatus : async (req, res)=> {
+        try {
+            const {id} = req.params;
+            const voucher = await Voucher.findOne({_id: id})
+
+            let status = voucher.status === 'Y' ? 'N' : 'Y'
+
+            await Voucher.findOneAndUpdate({
+                _id: id
+            }, {status})
+            req.flash('alertMessage', "Berhasil Ubah Status Voucher")
+            req.flash('alertStatus', "success")
+
+            res.redirect('/voucher');
+
         } catch (err) {
             req.flash('alertMessage', `${err.message}`)
             req.flash('alertStatus', 'danger')
