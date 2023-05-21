9b01f628bd8ad20456ad5115871b51bc52a6608e
exca <excamuchlisandita@gmail.com>
2023-05-21 14:01:27 +0700

    (CRUD) Bank - Delete

--- app/bank/controller.js
+++ app/bank/controller.js
@@ -84,24 +84,24 @@ module.exports = {
         }
     },
 
-    // actionDelete : async (req, res)=> {
-    //     try {
-    //         const {id} = req.params;
+    actionDelete : async (req, res)=> {
+        try {
+            const {id} = req.params;
 
-    //         await Nominal.findOneAndRemove({
-    //             _id: id
-    //         });
+            await Bank.findOneAndRemove({
+                _id: id
+            });
 
-    //         req.flash('alertMessage', "Berhasil Menghapus Nominal")
-    //         req.flash('alertStatus', "success")
+            req.flash('alertMessage', "Berhasil Menghapus Bank")
+            req.flash('alertStatus', "success")
 
-    //         res.redirect('/nominal');
-    //     } catch (err) {
-    //         req.flash('alertMessage', `${err.message}`)
-    //         req.flash('alertStatus', 'danger')
-    //         res.redirect('/nominal')
-    //     }
-    // }
+            res.redirect('/bank');
+        } catch (err) {
+            req.flash('alertMessage', `${err.message}`)
+            req.flash('alertStatus', 'danger')
+            res.redirect('/bank')
+        }
+    }
 
 
 }
