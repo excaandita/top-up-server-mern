87ce5b4b9136745ec4bfb927fc80c7353d70832f
exca <excamuchlisandita@gmail.com>
2023-05-21 13:59:26 +0700

    (CRUD) Bank - Read and Upadte

--- app/bank/controller.js
+++ app/bank/controller.js
@@ -9,7 +9,6 @@ module.exports = {
             const alert = { message: alertMessage, status: alertStatus };
             const bank = await Bank.find()
 
-
             res.render('admin/bank/view_bank',{
                 bank,
                 alert
@@ -49,41 +48,41 @@ module.exports = {
         }
     },
 
-    // viewEdit : async (req, res)=> {
-    //     try {
-    //         const {id} = req.params
+    viewEdit : async (req, res)=> {
+        try {
+            const {id} = req.params
 
-    //         const nominal = await Nominal.findOne({_id: id})
+            const bank = await Bank.findOne({_id: id})
 
-    //         res.render('admin/nominal/edit', {
-    //             nominal
-    //         })
-    //     } catch (err) {
-    //         req.flash('alertMessage', `${err.message}`)
-    //         req.flash('alertStatus', 'danger')
-    //         res.redirect('/nominal')
-    //     }
-    // },
+            res.render('admin/bank/edit', {
+                bank
+            })
+        } catch (err) {
+            req.flash('alertMessage', `${err.message}`)
+            req.flash('alertStatus', 'danger')
+            res.redirect('/bank')
+        }
+    },
 
-    // actionEdit : async (req, res)=> {
-    //     try {
-    //         const {id} = req.params
-    //         const {coinName, coinQuantity, price} = req.body
+    actionEdit : async (req, res)=> {
+        try {
+            const {id} = req.params
+            const {name, nameBank, noRekening} = req.body
             
-    //         await Nominal.findOneAndUpdate(
-    //             {_id: id}, {coinName, coinQuantity, price});
+            await Bank.findOneAndUpdate(
+                {_id: id}, {name, nameBank, noRekening});
 
-    //         req.flash('alertMessage', "Berhasil Edit Nominal")
-    //         req.flash('alertStatus', "success")
+            req.flash('alertMessage', "Berhasil Edit Bank")
+            req.flash('alertStatus', "success")
             
-    //         res.redirect('/nominal');
+            res.redirect('/bank');
             
-    //     } catch (err) {
-    //         req.flash('alertMessage', `${err.message}`)
-    //         req.flash('alertStatus', 'danger')
-    //         res.redirect('/nominal')
-    //     }
-    // },
+        } catch (err) {
+            req.flash('alertMessage', `${err.message}`)
+            req.flash('alertStatus', 'danger')
+            res.redirect('/bank')
+        }
+    },
 
     // actionDelete : async (req, res)=> {
     //     try {
